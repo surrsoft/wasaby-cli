4defd9aaa234f7b9b1206da7a5df67ac9327f643
ganshinyao <yo.ganshin@tensor.ru>
2019-07-30 10:57:06 +0300

    tslib path https://online.sbis.ru/opendoc.html?guid=ef29119e-a7d3-4bc2-89ff-83a7561a7a19

--- cli.js
+++ cli.js
@@ -567,6 +567,7 @@ class Cli {
       await this._execute(
          `node node_modules/gulp/bin/gulp.js --gulpfile=node_modules/sbis3-builder/gulpfile.js build --config=${this._buiderCfg}`,
          __dirname,
+         true,
          'builder'
       );
       fs.readdirSync(path.join(this._workDir, 'builder_test')).forEach(f => {
