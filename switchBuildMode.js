47898775ae2c776eb8eb0eeaf66fafb30779a6ce
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-07-31 16:52:44 +0300

    https://online.sbis.ru/opendoc.html?guid=713f6286-3f24-4371-b8cc-9fa85bf74d88

--- switchBuildMode.js
+++ switchBuildMode.js
@@ -1,5 +1,5 @@
 const setContents = require('./setContents');
 const pathResources = process.argv[2].replace('--resourcesRoot=', '');
-const value = process.argv[2].replace('--value=', '');
+const value = process.argv[3].replace('--value=', '');
 
 setContents(pathResources, value).then();
