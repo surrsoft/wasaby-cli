030acdb904ac13cb1b1fe5721c38a037d22bfb51
ganshinyao <yo.ganshin@tensor.ru>
2019-09-16 10:35:32 +0300

    https://online.sbis.ru/opendoc.html?guid=eae828d8-6fb2-4c14-b2fa-f413ce04ee69

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,17 +1,13 @@
 const chai = require('chai');
 const Cli = require('./../cli');
 const sinon = require('sinon');
-const fs = require('fs-extra');
-const path = require('path');
-const xml2js = require('xml2js');
 let cli;
 let stubArgv;
-let stubBuilder;
 
 describe('CLI', () => {
    beforeEach(() => {
       stubArgv = sinon.stub(process, 'argv').value(['','', '--rep=types', '--branch=200/feature', '--rc=rc-200']);
-      cli = new CliTest();
+      cli = new Cli();
    });
 
    afterEach(() => {
@@ -25,23 +21,5 @@ describe('CLI', () => {
          chai.expect(config).to.be.an('object').to.deep.equal({rep:'12',b:'15'});
       });
    });
-
-   // describe('.init()', () => {
-   //    it('should throw error when rep is empty', () => {
-   //       stubArgv.value(['','']);
-   //       chai.expect(() => cli.init()).to.throw();
-   //    });
-   //    it('should set params from argv', () => {
-   //       chai.expect(cli._testBranch).to.equal('200/feature');
-   //       chai.expect(cli._testRep[0]).to.equal('types');
-   //       chai.expect(cli._rc).to.equal('rc-200');
-   //    });
-   //    it('should set params from config', () => {
-   //       const config = require('./../config.json');
-   //       chai.expect(cli._repos).to.deep.equal(config.repositories);
-   //       chai.expect(cli._store).to.equal(path.join(process.cwd(), config.store));
-   //       chai.expect(cli._workDir).to.equal(path.join(process.cwd(),config.workDir));
-   //    });
-   // });
 
 });
