44096dd46372b31718a9250acb897aaed9f6ee7e
ganshinyao <yo.ganshin@tensor.ru>
2019-09-18 13:33:07 +0300

    tslint warning https://online.sbis.ru/opendoc.html?guid=0fcea775-496b-4a68-aab3-4e55b70f81e2

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,12 +1,12 @@
-const chai = require("chai");
-const Cli = require("./../cli");
-const sinon = require("sinon");
+const chai = require('chai');
+const Cli = require('./../cli');
+const sinon = require('sinon');
 let cli;
 let stubArgv;
 
-describe("CLI", () => {
+describe('CLI', () => {
    beforeEach(() => {
-      stubArgv = sinon.stub(process, "argv").value(["", "", "--rep=types", "--branch=200/feature", "--rc=rc-200"]);
+      stubArgv = sinon.stub(process, 'argv').value(['', '', '--rep=types', '--branch=200/feature', '--rc=rc-200']);
       cli = new Cli();
    });
 
@@ -14,22 +14,22 @@ describe("CLI", () => {
       stubArgv.restore();
    });
 
-   describe("._getArgvOptions()", () => {
-      it("should return argv options", () => {
-         stubArgv.value(["", "", "--rep=12", "--b=15"]);
+   describe('._getArgvOptions()', () => {
+      it('should return argv options', () => {
+         stubArgv.value(['', '', '--rep=12', '--b=15']);
          let config = cli._getArgvOptions();
-         chai.expect(config).to.be.an("object").to.deep.equal({
-            b: "15",
-            rep: "12",
+         chai.expect(config).to.be.an('object').to.deep.equal({
+            b: '15',
+            rep: '12'
          });
       });
    });
 
-   describe(".constructor()", () => {
-      it("should trim repository name", () => {
-         stubArgv = stubArgv.value(["", "", "--rep=types, controls", "--branch=200/feature", "--rc=rc-200"]);
+   describe('.constructor()', () => {
+      it('should trim repository name', () => {
+         stubArgv = stubArgv.value(['', '', '--rep=types, controls', '--branch=200/feature', '--rc=rc-200']);
          cli = new Cli();
-         chai.expect(cli._testRep).to.deep.equal(["types", "controls"]);
+         chai.expect(cli._testRep).to.deep.equal(['types', 'controls']);
       });
    });
 
