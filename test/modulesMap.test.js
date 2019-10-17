a5e96709146ed60ea18423c4f11c7c4341dbdf1b
ganshinyao <yo.ganshin@tensor.ru>
2019-10-17 09:36:07 +0300

    https://online.sbis.ru/opendoc.html?guid=a1c0a25a-e890-4733-bdd1-1c3222d77c1a

--- test/modulesMap.test.js
+++ test/modulesMap.test.js
@@ -198,4 +198,42 @@ describe('modulesMap', () => {
          stubModulesMap.restore();
       });
    });
+
+   describe('_markModulesForTest()',() => {
+      let stubModulesMap;
+      let stubTestModulesMap;
+      beforeEach(() => {
+         stubModulesMap = sinon.stub(modulesMap, '_modulesMap').value(
+             new Map([
+                ['testModule', {name: 'test11', rep: 'test1', depends: ['test12']}],
+                ['justModule', {name: 'test12', rep: 'test1', depends: []}],
+                ['undependedModule', {name: 'test13', rep: 'test1', depends: []}]
+             ])
+         );
+         stubTestModulesMap = sinon.stub(modulesMap, '_testModulesMap').value(new Map([
+            ['test1', ['test11']]
+         ]));
+      });
+
+      it('should mark module as for test', () => {
+         modulesMap._markModulesForTest();
+         chai.expect(modulesMap.get('test12').forTests).to.be.true;
+      });
+
+
+      it('should not mark module as for test when module has not been depended on test module', () => {
+         modulesMap._markModulesForTest();
+         chai.expect(modulesMap.get('test13').forTests).to.be.undefined;
+      });
+
+      it('should mark module test module', () => {
+         modulesMap._markModulesForTest();
+         chai.expect(modulesMap.get('test11').forTests).to.be.true;
+      });
+
+      afterEach(() => {
+         stubModulesMap.restore();
+         stubTestModulesMap.restore();
+      });
+   });
 });
