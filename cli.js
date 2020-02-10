bd9bd2604de88887c18880e8d99f9ab0dfa67a0d
ganshinyao <yo.ganshin@tensor.ru>
2020-02-10 10:30:03 +0300

    switch off check leaks https://online.sbis.ru/opendoc.html?guid=c4fafeed-a94c-441d-be94-9a838bfdf847

--- cli.js
+++ cli.js
@@ -110,7 +110,8 @@ class Cli {
          server: !!this._argvOptions.server,
          rc: this._rc,
          diff: this._argvOptions.diff,
-         coverage: this._argvOptions.coverage
+         coverage: this._argvOptions.coverage,
+         ignoreLeaks: this._argvOptions.ignoreLeaks
       });
 
       await test.run();
