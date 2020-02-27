6cc4ef354a4f5d1e755b06b2fd00c36040df6210
ganshinyao <yo.ganshin@tensor.ru>
2020-02-27 10:49:54 +0300

    call execute again and again https://online.sbis.ru/opendoc.html?guid=897bdf25-e997-44b0-90b8-333793856556

--- test/test.test.js
+++ test/test.test.js
@@ -328,7 +328,7 @@ describe('Test', () => {
    describe('._executeBrowserTestCmd()', () => {
       it('should call _executeBrowserTestCmd twice',() => {
          let spy = sinon.spy(test, '_executeBrowserTestCmd');
-         spy.callsFake(() => {
+         stubExecute.callsFake(() => {
             stubExecute.callsFake(() => Promise.resolve());
             return Promise.reject(['ECHROMEDRIVER']);
          });
