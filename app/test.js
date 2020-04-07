ded79d51c5c37d9cf2194f768710fb753b9ec308
Ярослав <t@tt.ru>
2020-04-07 09:58:44 +0300

    fiter errors without stack https://online.sbis.ru/opendoc.html?guid=8e8961a3-a598-4667-af3d-f5d4a5fe1365

--- app/test.js
+++ app/test.js
@@ -365,8 +365,9 @@ class Test extends Base {
          } finally {
             this._testErrors[processName] = this._testErrors[processName] ||  this._shell.getErrorsByName(processName);
             //todo разобраться почему ошибки без стека, пока такие не учитываем
-            this._testErrors[processName] = this._testErrors[processName].filter(msg => msg.includes('Stack:'));
-
+            if (this._testErrors[processName]) {
+               this._testErrors[processName] = this._testErrors[processName].filter(msg => msg.includes('Stack:'));
+            }
             if (this._shouldUpdateAllowedErrors) {
                this._testErrors[processName].map((msg) => {
                   this._allowedErrorsSet.add(this._getErrorText(msg));
