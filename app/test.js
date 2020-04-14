1339dbbfa8ecd6e5489f1fda2031f0211d5ec146
Ярослав <t@tt.ru>
2020-04-14 15:06:19 +0300

    https://online.sbis.ru/opendoc.html?guid=5fc3fee7-89d4-401c-a794-2472ca671970

--- app/test.js
+++ app/test.js
@@ -144,7 +144,7 @@ class Test extends Base {
                let result = xmlObject;
                if (result.testsuite && result.testsuite.testcase) {
                   result.testsuite.testcase.forEach((item) => {
-                     item.$.classname = `UnitTests.${name} ${item.$.classname.replace(/\./g, ' ')}`;
+                     item.$.classname = `UnitTests/${name}/${item.$.classname.replace(/\./g, ' ')}`;
                   });
                } else {
                   result = {
